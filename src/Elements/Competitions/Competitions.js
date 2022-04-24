24f30794ef55ac2f4ed48b8c97c66d6841512559
alfredomarcano <alfredomarcan@gmail.com>
2022-04-24 13:43:19 -0400

    error de tipeo

--- src/Elements/Competitions/Competitions.js
+++ src/Elements/Competitions/Competitions.js
@@ -4,7 +4,6 @@ import "../stylesheels/Competitions.css";
 export function Competitions() {
   return (
     <div className="Competiciones">
-<<<<<<< HEAD
       <div className='Compet-event'>
         <img src={require("./imagenes/Adrenalina-up.png")} alt="" width="100%" height="100%"/>
         <div>
@@ -53,9 +52,6 @@ export function Competitions() {
           <h3>Nombre del evento</h3>
         </div>
       </div>
-=======
-      competitions
->>>>>>> 2264153dd2c887eac761617c9ed04c5d8bf2f96b
     </div>
   );
 }
